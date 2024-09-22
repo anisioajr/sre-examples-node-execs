33bf800cf87374b8cbf8510d2c02112bba5099b4
anisioajr <anisioajr@gmail.com>
2024-09-22 12:35:06 -0300

    Create server-timeout.js

---
+++ server-timeout.js
@@ -0,0 +1,54 @@
+const express = require('express');
+
+const app = express();
+const port = 8080;
+
+// Função para criar uma Promise que simula um timeout
+function timeoutPromise(ms, promise) {
+    return new Promise((resolve, reject) => {
+        const timeout = setTimeout(() => {
+            reject(new Error('Tempo limite excedido!'));
+        }, ms);
+
+        promise
+            .then((result) => {
+                clearTimeout(timeout);
+                resolve(result);
+            })
+            .catch((error) => {
+                clearTimeout(timeout);
+                reject(error);
+            });
+    });
+}
+
+// Função simulando chamada externa
+async function externalService() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            // tempo que a simulação vai demorar pra responder
+            resolve('Resposta da chamada externa');
+        }, 2900); 
+    });
+}
+
+// Rota de health check
+app.get('/api/health', (req, res) => {
+    res.send('OK');
+});
+
+// Rota que faz a chamada simulada com timeout
+app.get('/api/timeout', async (req, res) => {
+    try {
+        // configuração do timeout-limit
+        const result = await timeoutPromise(3000, externalService());
+        res.send(result);
+    } catch (error) {
+        res.status(500).send(`Erro: ${error.message}`);
+    }
+});
+
+// Iniciando o servidor
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
